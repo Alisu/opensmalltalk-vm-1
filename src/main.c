f5d645259d164e986a018d492e15595a12ace915
Alisu <theorogliano@hotmail.com>
2019-10-03 15:34:24 +0200

    Refactor in order to use windows thread

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 #include <pharoClient.h>
 #include <pharo.h>
 
+#define MAX_THREADS 1
 
 int loadAndExecuteVM(VM_PARAMETERS* parameters){
 	if(!initPharoVM(parameters->imageFile, parameters->vmParams, parameters->vmParamsCount, parameters->imageParams, parameters->imageParamsCount)){
@@ -20,38 +22,54 @@ int runThread(void* parameters){
 
 
 int main(int argc, char* argv[]){
-	pthread_t thread_id;
+    WORD   dwThreadIdArray;
+	HANDLE  hThreadArray[MAX_THREADS];
 
-	VM_PARAMETERS parameters;
+	VM_PARAMETERS* parameters;
 	char buffer[4096+1];
 
-	parseArguments(argc, argv, &parameters);
+	parameters = (VM_PARAMETERS*) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
+                sizeof(VM_PARAMETERS));
 
-	logInfo("Opening Image: %s\n", parameters.imageFile);
+        if( parameters == NULL )
+        {
+           // If the array allocation fails, the system is out of memory
+           // so there is no point in trying to print an error message.
+           // Just terminate execution.
+            ExitProcess(2);
+        }
+
+	parseArguments(argc, argv, parameters);
+
+	logInfo("Opening Image: %s\n", parameters->imageFile);
 
 	//This initialization is required because it makes awful, awful, awful code to calculate
 	//the location of the machine code.
 	//Luckily, it can be cached.
-	osCogStackPageHeadroom();
+	//osCogStackPageHeadroom();
 
-	pthread_attr_t tattr;
+	hThreadArray[0] = CreateThread( 
+            NULL,                   // default security attributes
+            0,                      // use default stack size  
+            runThread,       // thread function name
+            parameters,          // argument to thread function 
+            0,                      // use default creation flags 
+            &dwThreadIdArray);
 
-	pthread_attr_init(&tattr);
+	if (hThreadArray[0] == NULL) 
+        {
+          // ErrorHandler(TEXT("CreateThread"));
+           ExitProcess(3);
+        }
 
-	size_t size;
-	pthread_attr_getstacksize(&tattr, &size);
+	WaitForMultipleObjects(MAX_THREADS, hThreadArray, TRUE, INFINITE);
 
-	printf("%ld\n", size);
-
-    if(pthread_attr_setstacksize(&tattr, size*4)){
-		perror("Thread attr");
-    }
-
-	if(pthread_create(&thread_id, &tattr, runThread, &parameters)){
-		perror("Thread creation");
-	}
-
-	pthread_join(thread_id, NULL);
+	CloseHandle(hThreadArray[0]);
+        if(parameters != NULL)
+        {
+            HeapFree(GetProcessHeap(), 0, parameters);
+            parameters = NULL;    // Ensure address is not reused.
+        }
 }
 
 void printVersion(){
